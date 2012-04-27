aae6d0c54927c13b12f9752e9025030e595ca4ea
Hernan Silva <hernandariosilva@gmail.com>
2012-04-27 01:47:02 -0500

    First commit of Seelva 2.x to GitHub

---
+++ config.rb
@@ -0,0 +1,10 @@
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+environment = :production
+output_style = :expanded
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
