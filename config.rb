fd90b1f2da2ee88df2d18dd7092a61b60b4c28e4
hachesilva <hachesilva@gmail.com>
2013-05-18 22:46:11 -0500

    First commit for v4.x

---
+++ config.rb
@@ -0,0 +1,11 @@
+css_dir = "css"
+sass_dir = "sass"
+fonts_dir = "fonts"
+images_dir = "images"
+javascripts_dir = "js"
+
+environment = :production
+output_style = :expanded
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
